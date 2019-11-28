f2d35b462804d19d7a47e86c4ec8f009032847b7
Yosep Ryan Agusta <yosep.ryan.yra@gmail.com>
2019-11-28 20:26:23 +0700

    Added contact form client-side validation

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Gloria Flower Shop
 
 A place to find the perfect flower for you.
+
+Visit [Live Site](https://yosepra.github.io/GloriaFlowerShop)
---
+++ dist/js/contact.js
@@ -0,0 +1,10 @@
+let form = document.querySelector('.contact-form form');
+let email = form['email'];
+
+email.addEventListener('input', () => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity('Please input a valid email address.');
+  } else {
+    email.setCustomValidity('');
+  }
+});
